97d6babd97e1d65349af44b15141e23c19960e91
USER <eugenemabansag8@gmail.com>
2026-03-02 03:33:27 +0800

    Changed codebase for future changes

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import random as rd
-# empty lists to store data
-data_set = []
-# final lists for the arranged data
-final_data_set = []
 
 # for loop to insert a random number of random data to the empty lists
-for i in range(rd.randint(1, 20)):
-    random_data = rd.randint(1, 20)
-    data_set.insert(i, random_data)
-
-# initial data set
-print(data_set)
-
-# data to be used as pivot
-pivot_data = data_set[rd.randint(1, len(data_set) - 1)]
+def setting_up_list(data_set):
+    for i in range(rd.randint(1, 20)):
+        random_data = rd.randint(1, 20)
+        data_set.insert(i, random_data)
+    return data_set
+    print(data_set)
 
 #function to seperate data based on chosen pivot
 def grouping_data(data_set, pivot_data):
@@ -31,25 +24,45 @@ def grouping_data(data_set, pivot_data):
     data_set = [smaller_group, equal_group, larger_group]
     return data_set
 
+# function to check for list with same elements
 def same_element_list(data_set):
     return len(set(data_set)) == 1 if data_set else True
 
-# re adjusting data set
-data_set = grouping_data(data_set, pivot_data)
-
 # inserting lists that contains the pivot, equal groups and groups with only one element into the final lists
-for j in range(len(data_set)):
-    if pivot_data in data_set[j]:
-        for k in range(len(data_set[j])):
-            final_data_set.append(data_set[j][k])
-    elif len(data_set[j]) < 1:
-        for k in range(len(data_set[j])):
-            final_data_set.append(data_set[j][k])
-    elif same_element_list(data_set[j]):
-        for k in range(len(data_set[j])):
-            final_data_set.append(data_set[j][k])
-    else:
-        pass
-# printing the data set after re adjustment
-print(data_set)
-print(final_data_set)
+def inserting_into_final_list(data_set, pivot_data):
+    for j in range(len(data_set)):
+        print(data_set[j])
+        if pivot_data in data_set[j]:
+            for k in range(len(data_set[j])):
+                final_data_set.append(data_set[j][k])
+        elif len(data_set[j]) < 1:
+            for k in range(len(data_set[j])):
+                final_data_set.append(data_set[j][k])
+        elif same_element_list(data_set[j]):
+            for k in range(len(data_set[j])):
+                final_data_set.append(data_set[j][k])
+        else:
+            pass
+
+if __name__ == "__main__":
+    # empty lists to store data
+    data_set = []
+
+    # final lists for the arranged data
+    final_data_set = []
+
+    # setting up list function
+    data_set = setting_up_list(data_set)
+
+    # data to be used as pivot
+    pivot_data = data_set[rd.randint(1, len(data_set) - 1)]
+
+    # group data into sub lists
+    data_set = grouping_data(data_set, pivot_data)
+
+    # inserting data into the final list through a function
+    inserting_into_final_list(data_set, pivot_data)
+
+    # printing the data set after re adjustment
+    print(data_set)
+    print(final_data_set)
